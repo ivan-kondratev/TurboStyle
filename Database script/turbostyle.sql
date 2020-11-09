3c434283e451c88cf23d75855f462011805975aa
Кондратьев Иван <vk15555555@outlook.com>
2020-11-09 19:09:55 +0300

    Обновил скрипт базы данных с добавленными данными

--- Database script/turbostyle.sql
+++ Database script/turbostyle.sql
@@ -1,88 +1,144 @@
--- MySQL Workbench Forward Engineering
+CREATE DATABASE  IF NOT EXISTS `turbostyle` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `turbostyle`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: turbostyle
+-- ------------------------------------------------------
+-- Server version	8.0.22
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
--- -----------------------------------------------------
--- Schema turbostyle
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `turbostyle` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `turbostyle` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Table `turbostyle`.`statuses`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `turbostyle`.`statuses` (
-  `statusID` INT(11) NOT NULL,
-  `statusName` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`statusID`),
-  UNIQUE INDEX `statusID_UNIQUE` (`statusID` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+--
+-- Table structure for table `inspections`
+--
 
-
--- -----------------------------------------------------
--- Table `turbostyle`.`requests`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `turbostyle`.`requests` (
-  `requestID` INT(11) NOT NULL AUTO_INCREMENT,
-  `statusID` INT(11) NOT NULL,
-  `CarMark` VARCHAR(30) NOT NULL,
-  `CarModel` VARCHAR(30) NOT NULL,
-  `CarVIN` VARCHAR(30) NOT NULL,
-  `LastName` VARCHAR(30) NOT NULL,
-  `FirstName` VARCHAR(30) NOT NULL,
-  `Patronymic` VARCHAR(30) NULL DEFAULT NULL,
-  `PhoneNumber` VARCHAR(14) NOT NULL,
-  PRIMARY KEY (`requestID`),
-  UNIQUE INDEX `requestID_UNIQUE` (`requestID` ASC) VISIBLE,
-  INDEX `status_fk_idx` (`statusID` ASC) VISIBLE,
-  CONSTRAINT `req_st`
-    FOREIGN KEY (`statusID`)
-    REFERENCES `turbostyle`.`statuses` (`statusID`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `turbostyle`.`inspections`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `turbostyle`.`inspections` (
-  `resultID` INT(11) NOT NULL AUTO_INCREMENT,
-  `result` VARCHAR(1000) NOT NULL,
-  `requestID` INT(11) NOT NULL,
-  `decision` TINYINT(1) NOT NULL,
+DROP TABLE IF EXISTS `inspections`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inspections` (
+  `resultID` int NOT NULL AUTO_INCREMENT,
+  `result` varchar(1000) NOT NULL,
+  `requestID` int NOT NULL,
+  `decision` tinyint(1) NOT NULL,
   PRIMARY KEY (`resultID`),
-  UNIQUE INDEX `resultID_UNIQUE` (`resultID` ASC) VISIBLE,
-  INDEX `ins_req_idx` (`requestID` ASC) VISIBLE,
-  CONSTRAINT `ins_req`
-    FOREIGN KEY (`requestID`)
-    REFERENCES `turbostyle`.`requests` (`requestID`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 11
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  UNIQUE KEY `resultID_UNIQUE` (`resultID`),
+  KEY `ins_req_idx` (`requestID`),
+  CONSTRAINT `ins_req` FOREIGN KEY (`requestID`) REFERENCES `requests` (`requestID`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `inspections`
+--
 
--- -----------------------------------------------------
--- Table `turbostyle`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `turbostyle`.`users` (
-  `userID` INT(11) NOT NULL AUTO_INCREMENT,
-  `userLogin` VARCHAR(16) NOT NULL,
-  `userPassword` VARCHAR(32) NOT NULL,
-  `userType` CHAR(1) NOT NULL,
+LOCK TABLES `inspections` WRITE;
+/*!40000 ALTER TABLE `inspections` DISABLE KEYS */;
+INSERT INTO `inspections` VALUES (11,'Осмотр кузова:\nДве царапины на правой стороне бампера\nНаличие критических дефектов: нет\n\nОсмтор двигателя и ходовой:\nВсе в норме\nНаличие критических дефектов: нет\n\nОсмотр электрики и диагностика:\nВсе в норие\nНаличие критических дефектов: нет\n\n',3,1);
+/*!40000 ALTER TABLE `inspections` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `requests`
+--
+
+DROP TABLE IF EXISTS `requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `requests` (
+  `requestID` int NOT NULL AUTO_INCREMENT,
+  `statusID` int NOT NULL,
+  `CarMark` varchar(30) NOT NULL,
+  `CarModel` varchar(30) NOT NULL,
+  `CarVIN` varchar(30) NOT NULL,
+  `LastName` varchar(30) NOT NULL,
+  `FirstName` varchar(30) NOT NULL,
+  `Patronymic` varchar(30) DEFAULT NULL,
+  `PhoneNumber` varchar(14) NOT NULL,
+  PRIMARY KEY (`requestID`),
+  UNIQUE KEY `requestID_UNIQUE` (`requestID`),
+  KEY `status_fk_idx` (`statusID`),
+  CONSTRAINT `req_st` FOREIGN KEY (`statusID`) REFERENCES `statuses` (`statusID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `requests`
+--
+
+LOCK TABLES `requests` WRITE;
+/*!40000 ALTER TABLE `requests` DISABLE KEYS */;
+INSERT INTO `requests` VALUES (3,0,'Nissan','GT-R','23213217821313231','Кондратьев','Иван','Сергеевич','(963) 960-4445');
+/*!40000 ALTER TABLE `requests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `statuses`
+--
+
+DROP TABLE IF EXISTS `statuses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `statuses` (
+  `statusID` int NOT NULL,
+  `statusName` varchar(10) NOT NULL,
+  PRIMARY KEY (`statusID`),
+  UNIQUE KEY `statusID_UNIQUE` (`statusID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `statuses`
+--
+
+LOCK TABLES `statuses` WRITE;
+/*!40000 ALTER TABLE `statuses` DISABLE KEYS */;
+INSERT INTO `statuses` VALUES (0,'wait'),(1,'work'),(2,'completed'),(3,'refusal');
+/*!40000 ALTER TABLE `statuses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `userLogin` varchar(16) NOT NULL,
+  `userPassword` varchar(32) NOT NULL,
+  `userType` char(1) NOT NULL,
   PRIMARY KEY (`userID`),
-  UNIQUE INDEX `userID_UNIQUE` (`userID` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  UNIQUE KEY `userID_UNIQUE` (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `users`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (5,'kondratev','12345','a'),(6,'qu_ckk','qwerty','s');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-09 19:08:31
